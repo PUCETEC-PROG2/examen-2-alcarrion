c19734ab226160ec89f2873688dce677a1413a6e
Ariel Umatambo <victorarielcr7rm@gmail.com>
2024-07-13 10:30:03 -0500

    subo todo

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    genre = models.CharField(max_length=30, null= False)
+    direct = models.CharField(max_length=30, null= False)
+    year = models.IntegerField()
+    synopsis = models.TextField(max_length=1000, null= False)
+    
+    def __srt__(self) -> str:
+        return self.title
+    
+    
+
